b422d08304dd3e90475ea40a6059f6fd264115ba
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-03-27 15:37:40 -0500

    Se implementó el método para agregar nuevas tareas

--- lista_tareas_app/servicios/tarea_servicio.py
+++ lista_tareas_app/servicios/tarea_servicio.py
@@ -10,3 +10,13 @@ class TareaServicio:
     def __init__(self):
         self._lista_tareas = []
         self._siguiente_identificador = 1
+
+    def agregar_tarea(self, descripcion: str) -> Tarea:
+        """
+        Crea una nueva tarea, la guarda en memoria y la retorna.
+        """
+        nueva_tarea = Tarea(self._siguiente_identificador, descripcion)
+        self._lista_tareas.append(nueva_tarea)
+        self._siguiente_identificador += 1
+        return nueva_tarea
+    
